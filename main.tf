cc08074a54baffba06dc809dc59f9a82d71b8679
Knightfury786 <mohdsiraj.dev@gmail.com>
2025-05-06 00:18:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "sample_bucket" {
-  bucket = "jenkins-terraform-bucket12"
+resource "aws_s3_bucket" "sample1" {
+  bucket = "my-jenkins-terraform-bucket32"
   acl    = "private"
 }
