72442610253ca677ccc52fccb3a150c65b65f4c7
Knightfury786 <mohdsiraj.dev@gmail.com>
2025-05-05 14:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "eu-central-1"
+}
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "jenkins-terraform-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
